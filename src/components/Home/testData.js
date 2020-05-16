67e2a6acf66c891a62dd05f9490e162c46ff748b
Tung Nguyen <ns.tung@outlook.com>
2020-05-16 18:17:30 +0700

    first feak

---
+++ src/components/Home/testData.js
@@ -0,0 +1,24 @@
+export default [{
+    "_id": "ce2690f9d7c34542a708d624a079a932",
+    "title": "Cựu thượng tá công an: Gia đình tôi sẽ kêu oan nhiều đời",
+    "thumb_art": "https://vcdn1-vnexpress.vnecdn.net/2020/05/16/IMG0101JPG43461589254583-15896-5964-4676-1589611819.jpg?w=680&h=408&q=100&dpr=1&fit=crop&s=Z5a8L6cETh-de9oA4sb9Uw",
+    "time": "Thứ bảy, 16/5/2020, 12:55 (GMT+7)",
+    "description": "Hòa BìnhBị cáo Khương Ngọc Chất (cựu thượng tá, cựu trưởng Phòng An ninh chính trị nội bộ, Công an tỉnh) nói sẽ kêu oan đến đời con, đời cháu trong vụ án nâng điểm thi.",
+    "categoryName": "Trangnhat"
+    },
+    {
+    "_id": "adb9cd03d23a65a5373fb3f962cf8415",
+    "title": "Sao Việt diện áo tắm đón hè",
+    "thumb_art": "https://vcdn1-giaitri.vnecdn.net/2020/05/16/IMG5232JPG-1589625680-8648-1589625687.jpg?w=220&h=132&q=100&dpr=1&fit=crop&s=KaAS9OV2JPZV3cNYx4IRHg",
+    "time": "Thứ bảy, 16/5/2020, 14:35 (GMT+7)",
+    "description": "Mai Phương Thúy, Minh Tú, Minh Triệu... tôn hình thể trong những bộ bikini, áo tắm một mảnh.",
+    "categoryName": "Trangnhat"
+    },
+    {
+    "_id": "66547a09b43255415305c99e23190cf2",
+    "title": "Phố đi bộ hồ Gươm nhộn nhịp trở lại",
+    "thumb_art": "https://vcdn1-vnexpress.vnecdn.net/2020/05/16/top-1589558377.jpg?w=220&h=132&q=100&dpr=1&fit=crop&s=1b3CFxTRxGv15EViPq2rAA",
+    "time": "Thứ bảy, 16/5/2020, 05:44 (GMT+7)",
+    "description": "Hà NộiSau hơn ba tháng dừng hoạt động để phòng chống Covid-19, phố đi bộ hồ Gươm đông đúc trong buổi tối 15/5.",
+    "categoryName": "Trangnhat"
+    }]
